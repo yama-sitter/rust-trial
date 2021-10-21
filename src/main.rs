1d6305b523038c335790c940db411f17d3507b88
dadayama <dadayamism@gmail.com>
2021-10-21 22:08:57 +0900

    lesson: 14

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod vars;
 // mod stack_heap;
 // mod ownership;
-mod lifetime;
+// mod lifetime;
+mod generics;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
     // stack_heap::run();
     // ownership::run();
-    lifetime::run();
+    // lifetime::run();
+    generics::run();
 }
